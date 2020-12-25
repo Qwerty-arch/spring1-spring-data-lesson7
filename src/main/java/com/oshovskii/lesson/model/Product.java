f2100b1c2f689297fbc7e19a3c3d21052289e32e
Qwerty <oshovsky.vlad@yandex.ru>
2020-12-26 00:45:42 +0800

    Commit 2

--- src/main/java/com/oshovskii/lesson/model/Product.java
+++ src/main/java/com/oshovskii/lesson/model/Product.java
@@ -1,4 +1,30 @@
 package com.oshovskii.lesson.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "id")
+        private Long id;
+
+        @Column(name = "title")
+        private String title;
+
+        @Column(name = "price")
+        private int price;
+
+        public Product(String title, int price) {
+                this.title = title;
+                this.price = price;
+        }
 }
---
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -0,0 +1,22 @@
+create table products (id bigserial primary key, title varchar(255), price int);
+insert into products (title, price) values
+('american bobtail cat', 10000),
+('bengal cat', 20000),
+('sphynx cat', 15000),
+('norwegian forest cat', 50000),
+('pixie-bob cat', 30000),
+('maine coon cat', 30000),
+('snowshoe cat', 30000),
+('persian cat', 30000),
+('donskoy cat', 30000),
+('savannah cat', 500000),
+('bombay cat', 14300),
+('european shorthair cat', 29000),
+('york chocolate cat', 44000),
+('korat cat', 50000),
+('pixie-bob cat', 55000),
+('napoleon cat', 30500),
+('nebelung cat', 30001),
+('sokoke cat', 30003),
+('chartreux cat', 40004),
+('birman cat', 50000);
